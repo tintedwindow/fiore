de76fe867b65dff9a037a957f03b45630a6b4427
tintedwindow <w41k3rar@gmail.com>
2024-03-26 19:27:48 +0530

    Add template auto reolading

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ from flask import Flask, flash, redirect, render_template, request
 # Configuring the application
 app = Flask(__name__)
 
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+
 
 @app.route("/")
 def index():

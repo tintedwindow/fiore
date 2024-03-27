71442f64ace7201a4a8687206b4c74ea1b53d82c
tintedwindow <w41k3rar@gmail.com>
2024-03-27 22:54:33 +0530

    Add landing page

--- app.py
+++ app.py
@@ -7,7 +7,15 @@ app = Flask(__name__)
 
 app.config["TEMPLATES_AUTO_RELOAD"] = True
 
+@app.after_request
+def after_request(response):
+    """Ensure responses aren't cached"""
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("landing.html")

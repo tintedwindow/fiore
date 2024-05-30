2d7922b8682c39dca3d874196fcf2042bb0b9af8
tintedwindow <w41k3rar@gmail.com>
2024-05-31 00:03:13 +0530

    Nav via template

--- static/script.js
+++ static/script.js
@@ -165,4 +165,24 @@ document.addEventListener("DOMContentLoaded", function() {
             document.getElementById('image-page-info').submit();
         });
     });
+
+
+    // Navigation toggle
+    const navToggle = document.querySelector('.nav-toggle');
+    const navMenu = document.querySelector('.nav-inner-left');
+
+    navToggle.addEventListener('click', function() {
+        navToggle.classList.toggle('open');
+        navMenu.classList.toggle('nav-inner-left-open');
+    });
+
+    const dropdownActivator = document.querySelector(".dropdown-activator");
+    const dropdown = document.querySelector(".nav-list-item-dropdown");
+    const dropdownArrow = document.getElementById("italy-dropdown-arrow"); 
+
+    dropdownActivator.addEventListener('click', function (event) {
+        event.preventDefault();
+        dropdown.classList.toggle('nav-list-item-dropdown-visible');
+        dropdownArrow.classList.toggle('italy-dropdown-arrow-open');
+    });
 });
